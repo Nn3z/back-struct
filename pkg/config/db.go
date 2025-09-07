a942e7fa676050265afcf5b8cf8b783c2f6f56df
Nn3z <ddaevir19@icloud.com>
2025-09-06 19:38:06 -0600

    add Dockerfile, schema database and wait-for-it.sh script

--- pkg/config/db.go
+++ pkg/config/db.go
@@ -14,6 +14,7 @@ import (
 var db *sql.DB
 
 func ConnectDB() {
+	log.Println(os.Getenv("DB_URL"))
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -43,4 +44,3 @@ func GetDB() *sql.DB {
 	}
 	return db
 }
-
